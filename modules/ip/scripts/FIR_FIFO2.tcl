4a54e69158301d4ddd55cabd03d5b1adc4e37af4
Surinder Pal Singh <samrajdhillon2020@gmail.com>
2021-04-17 10:22:05 -0400

    LFSR_TB updated to incorporate 4 FIFO and load the incoming data @2000MHz into 4 separate register that will hold the data for 2ns

---
+++ modules/ip/scripts/FIR_FIFO2.tcl
@@ -0,0 +1,19 @@
+
+set ip_name "FIR_FIFO2"
+set ip_dir  "../modules/ip"
+
+create_ip -name fifo_generator -vendor xilinx.com -library ip -version 13.2 -module_name $ip_name -dir $ip_dir
+
+
+set_property -dict [list CONFIG.Input_Data_Width {16} \
+                         CONFIG.Input_Depth {131072} \
+                         CONFIG.Output_Data_Width {16} \
+                         CONFIG.Output_Depth {131072} \
+                         CONFIG.Almost_Full_Flag {true} \
+                         CONFIG.Data_Count {true} \
+                         CONFIG.Data_Count_Width {17} \
+                         CONFIG.Write_Data_Count_Width {17} \
+                         CONFIG.Read_Data_Count_Width {17} \
+                         CONFIG.Full_Threshold_Assert_Value {131070} \
+                         CONFIG.Full_Threshold_Negate_Value {131069}] [get_ips $ip_name]
+
--- src/hdl/LFSR_TB.vhd
+++ src/hdl/LFSR_TB.vhd
@@ -9,14 +9,83 @@ USE ieee.std_logic_unsigned.ALL;
 USE work.fir_filter_pkg.ALL;
 
 LIBRARY unisim;
-
 USE unisim.vcomponents.ALL;
 
 ENTITY LFSR_TB IS
+  --  GENERIC (
+  --    SIM_MODE : STRING := "FALSE";
+  --    g_Num_Bits : INTEGER := 16
+  --  );
+  --  PORT (
+  --    --    FPGA_CLK_P : IN STD_LOGIC;
+  --    --    FPGA_CLK_N : IN STD_LOGIC;
+  --    ADC_CLK_P : IN STD_LOGIC;
+  --    ADC_CLK_N : IN STD_LOGIC;
+  --    fir_data_out_IO_P : OUT STD_LOGIC;    -- _VECTOR(g_NUM_Bits-1 DOWNTO 0);
+  --    fir_data_out_IO_N : OUT STD_LOGIC    -- _VECTOR(g_NUM_Bits-1 DOWNTO 0)
+
+  --    --    DAC_CLK_P : IN STD_LOGIC;
+  --    --    DAC_CLK_N : IN STD_LOGIC
+  --  );
 END ENTITY LFSR_TB;
 
 ARCHITECTURE behave OF LFSR_TB IS
-
+  COMPONENT FIR_FIFO1
+    PORT (
+      clk : IN STD_LOGIC;
+      srst : IN STD_LOGIC;
+      din : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+      wr_en : IN STD_LOGIC;
+      rd_en : IN STD_LOGIC;
+      dout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+      full : OUT STD_LOGIC;
+      almost_full : OUT STD_LOGIC;
+      empty : OUT STD_LOGIC;
+      data_count : OUT STD_LOGIC_VECTOR(16 DOWNTO 0)
+    );
+  END COMPONENT;
+  COMPONENT FIR_FIFO2
+    PORT (
+      clk : IN STD_LOGIC;
+      srst : IN STD_LOGIC;
+      din : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+      wr_en : IN STD_LOGIC;
+      rd_en : IN STD_LOGIC;
+      dout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+      full : OUT STD_LOGIC;
+      almost_full : OUT STD_LOGIC;
+      empty : OUT STD_LOGIC;
+      data_count : OUT STD_LOGIC_VECTOR(16 DOWNTO 0)
+    );
+  END COMPONENT;
+  COMPONENT FIR_FIFO3
+    PORT (
+      clk : IN STD_LOGIC;
+      srst : IN STD_LOGIC;
+      din : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+      wr_en : IN STD_LOGIC;
+      rd_en : IN STD_LOGIC;
+      dout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+      full : OUT STD_LOGIC;
+      almost_full : OUT STD_LOGIC;
+      empty : OUT STD_LOGIC;
+      data_count : OUT STD_LOGIC_VECTOR(16 DOWNTO 0)
+    );
+  END COMPONENT;
+  COMPONENT FIR_FIFO4
+    PORT (
+      clk : IN STD_LOGIC;
+      srst : IN STD_LOGIC;
+      din : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+      wr_en : IN STD_LOGIC;
+      rd_en : IN STD_LOGIC;
+      dout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+      full : OUT STD_LOGIC;
+      almost_full : OUT STD_LOGIC;
+      empty : OUT STD_LOGIC;
+      data_count : OUT STD_LOGIC_VECTOR(16 DOWNTO 0)
+    );
+  END COMPONENT;
   COMPONENT FIR_FILTER IS
     GENERIC (
       NUM_OF_TAPS : INTEGER := 4;
@@ -39,13 +108,28 @@ ARCHITECTURE behave OF LFSR_TB IS
       -- filtered data 
       o_data : OUT STD_LOGIC_VECTOR(C_DATA_OUT_LENGTH - 1 DOWNTO 0));
   END COMPONENT;
+  COMPONENT IBUFGDS
+    GENERIC (
+      CAPACITANCE : STRING := "DONT_CARE";
+      DIFF_TERM : BOOLEAN := FALSE;
+      IBUF_DELAY_VALUE : STRING := "0";
+      IBUF_LOW_PWR : BOOLEAN := TRUE;
+      IOSTANDARD : STRING := "DEFAULT");
+
+    PORT (
+      O : OUT STD_LOGIC;
+      I : IN STD_LOGIC;
+      IB : IN STD_LOGIC
+    );
+  END COMPONENT;
 
   CONSTANT c_NUM_BITS : INTEGER := 16;
   CONSTANT c_SEED_COUNTER_BITS : INTEGER := 16;
   CONSTANT c_CLK_PERIOD : TIME := 2 ns;
   SIGNAL fir_data_in : STD_LOGIC_VECTOR(c_NUM_BITS - 1 DOWNTO 0);
   SIGNAL fir_data_out : STD_LOGIC_VECTOR(c_NUM_BITS - 1 DOWNTO 0);
-  SIGNAL LFSR_Data_Q : STD_LOGIC_VECTOR(c_NUM_BITS - 1 DOWNTO 0);
+  SIGNAL fir_data_out_IO : STD_LOGIC_VECTOR(c_NUM_BITS - 1 DOWNTO 0);
+
   SIGNAL w_LFSR_Done : STD_LOGIC;
   SIGNAL i_Enable : STD_LOGIC;
 
@@ -75,70 +159,149 @@ ARCHITECTURE behave OF LFSR_TB IS
 
   SIGNAL MULT : MULT_TYPE;
   SIGNAL ADD : ADD_TYPE;
+
+  SIGNAL Valid_in : STD_LOGIC;
+  SIGNAL Valid_in_cnt : STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+  SIGNAL s_adc_data_Valid : STD_LOGIC_VECTOR(c_NUM_BITS - 1 DOWNTO 0);
+  -- fir fifo 
+
+  SIGNAL fifo1_din, fifo2_din, fifo3_din, fifo4_din : STD_LOGIC_VECTOR(c_NUM_BITS - 1 DOWNTO 0);
+  SIGNAL fifo1_wr_en, fifo2_wr_en, fifo3_wr_en, fifo4_wr_en : STD_LOGIC;
+  SIGNAL fifo1_rd_en, fifo2_rd_en, fifo3_rd_en, fifo4_rd_en : STD_LOGIC;
+  SIGNAL fifo1_dout, fifo2_dout, fifo3_dout, fifo4_dout : STD_LOGIC_VECTOR(c_NUM_BITS - 1 DOWNTO 0);
+  SIGNAL fifo1_rst, fifo2_rst, fifo3_rst, fifo4_rst : STD_LOGIC;
+
+  SIGNAL d1_strb_Q, d2_strb_Q, d3_strb_Q, d4_strb_Q : STD_LOGIC;
+  SIGNAL d1_strb_QQ, d2_strb_QQ, d3_strb_QQ, d4_strb_QQ : STD_LOGIC;
+  SIGNAL d1_Q, d2_Q, d3_Q, d4_Q : STD_LOGIC_VECTOR(c_NUM_BITS - 1 DOWNTO 0);
+
 BEGIN
   i_Seed_Data <= x"DEAD";
-
-
+  -- fir_data_out_IO <= fir_data_out;
+  --  G_DIFF_OBUG : FOR I IN 0 TO 0 GENERATE
+  --  DIFF_OBUF : OBUFDS PORT MAP (
+  --    I => fir_data_out_IO(I),
+  --    O => fir_data_out_IO_P,
+  --    OB => fir_data_out_IO_N    
+  --  );
+  --  END GENERATE G_DIFF_OBUG;
   -- GENERATE SINGLE ENDED ADC CLK FROM DIFFERENTIAL INPUT
 
   adc_ibuf : IBUFGDS
-  GENERIC MAP (
+  GENERIC MAP(
     DIFF_TERM => FALSE,
-    IBUF_LOW_PWR =>  FALSE)
-
-  PORT MAP (
+    IBUF_LOW_PWR => FALSE)
+  PORT MAP(
     O => adc_clk,
     I => INT_ADC_CLK_P,
     IB => INT_ADC_CLK_N
   );
 
+  -- GENERATE SINGLE ENDED dac CLK FROM DIFFERENTIAL INPUT
+  dac_clk_ibuf : IBUFGDS
+  GENERIC MAP(
+    DIFF_TERM => FALSE,
+    IBUF_LOW_PWR => FALSE)
+  PORT MAP(
+    O => dac_clk,
+    I => INT_DAC_CLK_P,
+    IB => INT_DAC_CLK_N
+  );
 
+  -- GENERATE SINGLE ENDED fpga CLK FROM DIFFERENTIAL INPUT
+  fpga_clk_ibuf : IBUFGDS
+  GENERIC MAP(
+    DIFF_TERM => FALSE,
+    IBUF_LOW_PWR => FALSE)
+  PORT MAP(
+    O => fpga_clk,
+    I => INT_FPGA_CLK_P,
+    IB => INT_FPGA_CLK_N
+  );
+  INT_FPGA_CLK_P <= NOT INT_FPGA_CLK_P AFTER c_CLK_PERIOD/2;
+  INT_FPGA_CLK_N <= NOT INT_FPGA_CLK_P;
+  INT_ADC_CLK_P <= NOT INT_ADC_CLK_P AFTER c_CLK_PERIOD/8;
+  INT_ADC_CLK_N <= NOT INT_ADC_CLK_P;
+  INT_DAC_CLK_P <= NOT INT_DAC_CLK_P AFTER c_CLK_PERIOD/8;
+  INT_DAC_CLK_N <= NOT INT_DAC_CLK_P;
+  fifo1_rst <= NOT rst_n;
+  fifo2_rst <= NOT rst_n;
+  fifo3_rst <= NOT rst_n;
+  fifo4_rst <= NOT rst_n;
 
-  -- adc_clk_ibug : unisim.vcomponents.IBUFDS
-  -- GENERIC MAP(
-  --   DIFF_TERM => TRUE,
-  --   DQS_BIAS => "FALSE",
-  --   IOSTANDARD => "DEFAULT"
-  -- )
-  -- PORT MAP(
-  --   O => adc_clk,
-  --   I => ADC_CLK_P,
-  --   IB => ADC_CLK_N
-  -- );
-  --  -- GENERATE SINGLE ENDED ADC CLK FROM DIFFERENTIAL INPUT
-  --  dac_clk_ibug : unisim.vcomponents.IBUFDS
-  --  GENERIC MAP(
-  --    DIFF_TERM => TRUE,
-  --    DQS_BIAS => "FALSE",
-  --    IOSTANDARD => "DEFAULT"
-  --  )
-  --  PORT MAP(
-  --    O => dac_clk,
-  --    I  => DAC_CLK_P,
-  --    IB => DAC_CLK_N
-  --  );
-  --  -- GENERATE SINGLE ENDED ADC CLK FROM DIFFERENTIAL INPUT
-  --  fpga_clk_ibug : unisim.vcomponents.IBUFDS
-  --  GENERIC MAP(
-  --    DIFF_TERM => TRUE,
-  --    DQS_BIAS => "FALSE",
-  --    IOSTANDARD => "DEFAULT"
-  --  )
-  --  PORT MAP(
-  --    O => fpga_clk,
-  --    I =>  FPGA_CLK_P,
-  --    IB => FPGA_CLK_N
-  --  );
+  U_FIR_FIFO1 : FIR_FIFO1
+  PORT MAP(
+    clk => fpga_clk,
+    srst => fifo1_rst,
+    din => fifo1_din,
+    wr_en => fifo1_wr_en,
+    rd_en => fifo1_rd_en,
+    dout => fifo1_dout,
+    full => OPEN,
+    almost_full => OPEN,
+    empty => OPEN,
+    data_count => OPEN
+  );
 
+  U_FIR_FIFO2 : FIR_FIFO2
+  PORT MAP(
+    clk => fpga_clk,
+    srst => fifo2_rst,
+    din => fifo2_din,
+    wr_en => fifo2_wr_en,
+    rd_en => fifo2_rd_en,
+    dout => fifo2_dout,
+    full => OPEN,
+    almost_full => OPEN,
+    empty => OPEN,
+    data_count => OPEN
+  );
 
-    INT_FPGA_CLK_P <= NOT INT_FPGA_CLK_P AFTER c_CLK_PERIOD/2;
-    INT_FPGA_CLK_N <= NOT INT_FPGA_CLK_P;
-    INT_ADC_CLK_P <= NOT INT_ADC_CLK_P AFTER c_CLK_PERIOD/8;
-    INT_ADC_CLK_N <= NOT INT_ADC_CLK_P;
-    INT_DAC_CLK_P <= NOT INT_DAC_CLK_P AFTER c_CLK_PERIOD/8;
-    INT_DAC_CLK_N <= NOT INT_DAC_CLK_P;
+  U_FIR_FIFO3 : FIR_FIFO3
+  PORT MAP(
+    clk => fpga_clk,
+    srst => fifo3_rst,
+    din => fifo3_din,
+    wr_en => fifo3_wr_en,
+    rd_en => fifo3_rd_en,
+    dout => fifo3_dout,
+    full => OPEN,
+    almost_full => OPEN,
+    empty => OPEN,
+    data_count => OPEN
+  );
 
+  U_FIR_FIFO4 : FIR_FIFO4
+  PORT MAP(
+    clk => fpga_clk,
+    srst => fifo4_rst,
+    din => fifo4_din,
+    wr_en => fifo4_wr_en,
+    rd_en => fifo4_rd_en,
+    dout => fifo4_dout,
+    full => OPEN,
+    almost_full => OPEN,
+    empty => OPEN,
+    data_count => OPEN
+  );
+  -- Generate Fake Valid strb every 16th sample
+  p_valid : PROCESS (adc_clk, rst_n)
+  BEGIN
+    IF (rst_n = '0') THEN
+      Valid_in <= '0';
+      Valid_in_cnt <= (OTHERS => '0');
+    ELSIF rising_edge(adc_clk) THEN
 
+      Valid_in <= '0';
+      IF (Valid_in_cnt = 0) THEN
+        Valid_in <= '1';
+        Valid_in_cnt <= (OTHERS => '1');
+      ELSE
+        Valid_in_cnt <= Valid_in_cnt - b"1";
+      END IF;
+    END IF;
+  END PROCESS p_valid;
 
   p_reset : PROCESS (adc_clk, stop_reset_timer)
   BEGIN
@@ -156,7 +319,7 @@ BEGIN
   BEGIN
     IF (rst_n = '0') THEN
       i_Seed_DV <= '1';
-      i_Enable <= '0';
+      i_Enable <= '0';      
     ELSIF rising_edge(adc_clk) THEN
       --default
       i_Seed_DV <= '0';
@@ -168,9 +331,43 @@ BEGIN
       ELSE
         seed_counter <= seed_counter - b"1";
       END IF;
-
     END IF;
   END PROCESS p_Seed;
+
+  p_adc_data : PROCESS (adc_clk, rst_n)
+  BEGIN
+    IF (rst_n = '0') THEN
+
+      s_adc_data_Valid <= (OTHERS => '0');
+    ELSIF rising_edge(adc_clk) THEN
+      -- adc data acquisation into fir fifo cntrl
+      IF (Valid_in = '1') THEN
+        s_adc_data_Valid <= x"0001";
+      ELSE
+        s_adc_data_Valid <= s_adc_data_Valid(c_NUM_BITS - 2 DOWNTO 0) & b"0";
+      END IF;
+
+      d1_strb_QQ <= d1_strb_Q;
+      d2_strb_QQ <= d2_strb_Q;
+      d3_strb_QQ <= d3_strb_Q;
+      d4_strb_QQ <= d4_strb_Q;
+
+      IF (d1_strb_Q = '1') THEN
+        d1_Q <= fir_data_in;
+      ELSIF (d2_strb_Q = '1') THEN
+        d2_Q <= fir_data_in;
+      ELSIF (d3_strb_Q = '1') THEN
+        d3_Q <= fir_data_in;
+      ELSIF (d4_strb_Q = '1') THEN
+        d4_Q <= fir_data_in;
+      END IF;
+    END IF;
+  END PROCESS p_adc_data;
+
+  d1_strb_Q <= (s_adc_data_Valid(0) OR s_adc_data_Valid(4) OR s_adc_data_Valid(8) OR s_adc_data_Valid(12));
+  d2_strb_Q <= s_adc_data_Valid(1) OR s_adc_data_Valid(5) OR s_adc_data_Valid(9) OR s_adc_data_Valid(13);
+  d3_strb_Q <= s_adc_data_Valid(2) OR s_adc_data_Valid(6) OR s_adc_data_Valid(10) OR s_adc_data_Valid(14);
+  d4_strb_Q <= s_adc_data_Valid(3) OR s_adc_data_Valid(7) OR s_adc_data_Valid(11) OR s_adc_data_Valid(15);
 
   LFSR_1 : ENTITY work.LFSR
     GENERIC MAP(
